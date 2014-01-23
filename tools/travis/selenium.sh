39870b99d50d0d160ec66bf2dae999169eb4419f
Jakub Zalas <jakub@zalas.pl>
2014-01-23 17:54:57 +0000

    Extracted a selenium runner script.

---
+++ tools/travis/selenium.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+command=$1||"start"
+
+SELENIUM_VERSION="2.39.0"
+
+download() {
+  [ -f selenium-server-standalone.jar ] || wget http://selenium.googlecode.com/files/selenium-server-standalone-${SELENIUM_VERSION}.jar -Oselenium-server-standalone.jar
+}
+
+start_hub() {
+  echo "Starting the hub"
+  java -jar selenium-server-standalone.jar -role hub &> /dev/null &
+}
+
+start_nodes() {
+  nodes=$(grep "^processor" /proc/cpuinfo | sort -u | wc -l)
+  for i in $(seq 1 $nodes); do
+    port=$(expr 5557 + $i)
+    echo "Starting node "$i
+    java -jar selenium-server-standalone.jar -role node -port $port -browser browserName=phantomjs &> /dev/null &
+    sleep 1
+  done
+}
+
+start() {
+  download
+  start_hub
+  start_nodes
+}
+
+stop() {
+  ps ax o'pid command' | grep selenium-server | grep -v grep | awk '{print $1}' | tr "\n" ' ' | xargs kill -9 &> /dev/null
+}
+
+$command
+
