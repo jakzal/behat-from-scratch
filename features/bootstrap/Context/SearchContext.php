4f513d5a5841545039889e8ae208c92d37012f3d
Jakub Zalas <jakub@zalas.pl>
2015-06-04 15:43:26 +0100

    Create an example of RestExtension usage

--- features/bootstrap/Context/SearchContext.php
+++ features/bootstrap/Context/SearchContext.php
@@ -3,25 +3,26 @@
 namespace Context;
 
 use Behat\Behat\Context\Context;
-use GuzzleHttp\ClientInterface;
-use GuzzleHttp\Message\ResponseInterface;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use Zalas\Behat\RestExtension\Http\HttpClient;
 
 class SearchContext implements Context
 {
     /**
-     * @var ClientInterface
+     * @var HttpClient
      */
     private $httpClient;
 
     /**
-     * @var ResponseInterface
+     * @var Response
      */
     private $lastResponse;
 
     /**
-     * @param ClientInterface $httpClient
+     * @param HttpClient $httpClient
      */
-    public function __construct(ClientInterface $httpClient)
+    public function __construct(HttpClient $httpClient)
     {
         $this->httpClient = $httpClient;
     }
@@ -31,7 +32,9 @@ class SearchContext implements Context
      */
     public function iSearchFor($postcode)
     {
-        $this->lastResponse = $this->httpClient->get('http://api.postcodes.io/postcodes/'.$postcode, ['exceptions' => false]);
+        $this->lastResponse = $this->httpClient->send(
+            new Request('GET', sprintf('http://api.postcodes.io/postcodes/%s', $postcode))
+        );
     }
 
     /**
@@ -39,9 +42,9 @@ class SearchContext implements Context
      */
     public function iShouldSeeItsLocation()
     {
-        $json = $this->lastResponse->json();
-
         \PHPUnit_Framework_Assert::assertSame(200, $this->lastResponse->getStatusCode(), 'Got a successful response');
+
+        $json = json_decode($this->lastResponse->getBody(), true);
         \PHPUnit_Framework_Assert::assertInternalType('array', $json, 'Response contains a query result');
         \PHPUnit_Framework_Assert::arrayHasKey('result', $json, 'Result found in the response');
         \PHPUnit_Framework_Assert::assertArrayHasKey('latitude', $json['result'], 'Latitude found in the response');
